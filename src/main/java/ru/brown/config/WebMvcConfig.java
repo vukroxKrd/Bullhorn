e63084a9fa16433bfdaf6feaf027d581de18fd72
vukrox <62335798+vukrox@users.noreply.github.com>
2021-10-02 11:47:00 +0300

    added Spring

---
+++ src/main/java/ru/brown/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package main.java.ru.brown.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("main.java.ru.brown")
+public class WebMvcConfig {
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
